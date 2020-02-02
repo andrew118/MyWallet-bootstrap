e9d75be599a88ba80bd63e58a39e6309b2a64e25
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-02-02 11:49:57 +0100

    Function adding current date. Separated file

---
+++ flow.js
@@ -0,0 +1,28 @@
+var today = new Date();
+
+function correct(number) {
+	var correctedNumber = "0";
+	
+	number = number.toString();
+	correctedNumber += number;
+	
+	return correctedNumber;
+}
+
+function putTodayDate () {
+	var year = today.getFullYear(); 
+	var month = today.getMonth() + 1;
+	var day = today.getDate();
+	
+	if (month < 9) {
+		month = correct(month);
+	}
+	if (day <= 9) {
+		day = correct(day);
+	}
+	
+	var wholeDate = year + "-" + month + "-" + day;
+	
+	document.getElementById("dater").value = wholeDate;
+}
+
